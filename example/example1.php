8facff484384bd658f807f1dc9635610a6291534
wangliangliang2 <wangliangliang2@qiniu.com>
2017-07-05 15:11:23 +0800

    fix RTC Client bug

--- example/example1.php
+++ example/example1.php
@@ -1,11 +1,8 @@
 <?php
-
 require(join(DIRECTORY_SEPARATOR, array(dirname(dirname(__FILE__)), 'lib', 'Pili_v2.php')));
-
 $ak = "Ge_kRfuV_4JW0hOCOnRq5_kD1sX53bKVht8FNdd3";
 $sk = "0fU92CSrvgNJTVCXqbuRVqkntPFJLFERGa4akpko";
 $hubName = "PiliSDKTest";
-
 //创建hub
 echo "================Create hub\n";
 $mac = new Qiniu\Pili\Mac($ak, $sk);
@@ -17,7 +14,6 @@ echo "================Get stream\n";
 $streamKey = "php-sdk-test" . time();
 $stream = $hub->stream($streamKey);
 print_r($stream);
-
 try {
     //创建stream
     echo "================Create stream\n";
@@ -41,7 +37,6 @@ try {
 } catch (\Exception $e) {
     echo "Error:", $e, "\n";
 }
-
 try {
     echo "================Get liveStatus:\n";
     $status = $stream->liveStatus();
@@ -49,7 +44,6 @@ try {
 } catch (\Exception $e) {
     echo "Error:", $e, "\n";
 }
-
 try {
     //禁用流
     echo "================Disable stream:\n";
@@ -63,7 +57,6 @@ try {
 } catch (\Exception $e) {
     echo "Error:", $e, "\n";
 }
-
 try {
     //启用流
     echo "================Enable stream:\n";
@@ -77,8 +70,6 @@ try {
 } catch (\Exception $e) {
     echo "Error:", $e, "\n";
 }
-
-
 try {
     //保存直播数据
     echo "================Save stream:\n";
@@ -87,7 +78,6 @@ try {
 } catch (\Exception $e) {
     echo "Error:", $e, "\n";
 }
-
 try {
     //保存直播数据
     echo "================Save stream:\n";
@@ -98,7 +88,6 @@ try {
 } catch (\Exception $e) {
     echo "Error:", $e, "\n";
 }
-
 try {
     //查询推流历史
     echo "================Get stream history record:\n";
@@ -107,7 +96,6 @@ try {
 } catch (\Exception $e) {
     echo "Error:", $e, "\n";
 }
-
 try {
     //保存直播截图
     echo "================Save snapshot:\n";
@@ -118,7 +106,6 @@ try {
 } catch (\Exception $e) {
     echo "Error:", $e, "\n";
 }
-
 try {
     //更改流的实时转码规格
     echo "================Update converts:\n";
@@ -132,7 +119,6 @@ try {
 } catch (\Exception $e) {
     echo "Error:", $e, "\n";
 }
-
 //RTMP 推流地址
 $url = Qiniu\Pili\RTMPPublishURL("publish-rtmp.test.com", $hubName, $streamKey, 3600, $ak, $sk);
 echo $url, "\n";
@@ -148,4 +134,3 @@ echo $url, "\n";
 //截图直播地址
 $url = Qiniu\Pili\SnapshotPlayURL("live-snapshot.test.com", $hubName, $streamKey);
 echo $url, "\n";
-
--- example/example2.php
+++ example/example2.php
@@ -2,13 +2,14 @@
 
 require(join(DIRECTORY_SEPARATOR, array(dirname(dirname(__FILE__)), 'lib', 'Pili_v2.php')));
 
-$ak = "Tn8WCjE_6SU7q8CO3-BD-yF4R4IZbHBHeL8Q9t";
-$sk = "vLZNvZDojo1F-bYOjOqQ43-NYqlKAej0e9OweInh";
+$ak="QzdCUKE0lXmIJsvJ_yQJTeIsJYeK6liEdWAn9JuU";
+$sk="HwIhvaYrUZ4pedSRTpKZsqunSty0uUpQdrFDGLGU";
 
 $mac = new Qiniu\Pili\Mac($ak, $sk);
 $client = new Qiniu\Pili\RoomClient($mac);
 
 try {
+   
     $resp = $client->createRoom("901", "testroom");
     print_r($resp);
 
@@ -24,6 +25,11 @@ try {
     print_r($resp);
     //鉴权的有效时间: 1个小时.
     $resp = $client->roomToken("testroom", "123", 'admin', (time()+3600));
+
+    print_r($resp);
+     $resp=$client->kickingPlayer("testroom","qiniu-f6e07b78-4dc8-45fb-a701-a9e158abb8e6");
+     print_r($resp);
+    $resp3=$client->getRoomUserNum("testroom");
     print_r($resp);
 } catch (\Exception $e) {
     echo "Error:", $e, "\n";
