cdcef854c23134228c4e4f0d40d06dc8aad3d8a7
sicong-Echo <sicong@sicong.local>
2017-12-07 11:53:36 +0800

    pili-v2

--- lib/Qiniu/Pili/RoomClient.php
+++ lib/Qiniu/Pili/RoomClient.php
@@ -1,27 +1,24 @@
 <?php
-
 namespace Qiniu\Pili;
-
 use \Qiniu\Pili\Utils;
-
 class RoomClient
 {
     private $_transport;
     private $_mac;
-
     public function __construct($mac)
     {
         $this->_mac = $mac;
         $this->_transport = new Transport($mac);
     }
-
     /*
      * ownerId: 要创建房间的所有者
      * roomName: 房间名称
+     * Version:连麦的版本号 支持 v2 和 v1 
      */
-    public function createRoom($ownerId, $roomName = NULL)
+    public function createRoom($ownerId, $roomName = NULL,$Version="v2")
     {
-        $url = Config::getInstance()->RTCAPI_HOST . "/v1/rooms";
+
+        $url = Config::getInstance()->RTCAPI_HOST . sprintf("/%s/rooms",$Version);
         $params['owner_id'] = $ownerId;
         if (!empty($roomName)) {
             $params['room_name'] = $roomName;
@@ -32,16 +29,15 @@ class RoomClient
         } catch (\Exception $e) {
             return $e;
         }
-
         return $ret;
     }
-
     /*
      * roomName: 房间名称
+     * Version:连麦的版本号
      */
-    public function getRoom($roomName)
+    public function getRoom($roomName,$Version="v2")
     {
-        $url = sprintf("%s/v1/rooms/%s", Config::getInstance()->RTCAPI_HOST, $roomName);
+        $url = sprintf("%s/%s/rooms/%s", Config::getInstance()->RTCAPI_HOST, $Version,$roomName);
         try {
             $ret = $this->_transport->send(HttpRequest::GET, $url);
         } catch (\Exception $e) {
@@ -49,13 +45,13 @@ class RoomClient
         }
         return $ret;
     }
-
     /*
      * roomName: 房间名称
+     * Version:连麦的版本号
      */
-    public function deleteRoom($roomName)
+    public function deleteRoom($roomName,$Version="v2")
     {
-        $url = sprintf("%s/v1/rooms/%s", Config::getInstance()->RTCAPI_HOST, $roomName);
+        $url = sprintf("%s/%s/rooms/%s", Config::getInstance()->RTCAPI_HOST,$Version ,$roomName);
         try {
             $ret = $this->_transport->send(HttpRequest::DELETE, $url);
         } catch (\Exception $e) {
@@ -63,7 +59,35 @@ class RoomClient
         }
         return $ret;
     }
-
+        /*
+     * 获取房间的人数
+     * roomName: 房间名称
+     * Version:连麦的版本号
+    */
+    public function getRoomUserNum($roomName,$Version="v2"){
+        $url = sprintf("%s/%s/rooms/%s/users", Config::getInstance()->RTCAPI_HOST,$Version, $roomName);
+        try {
+            $ret = $this->_transport->send(HttpRequest::GET, $url);
+        } catch (\Exception $e) {
+            return $e;
+        }
+        return $ret;
+    }
+    /*
+     * 踢出玩家
+    * roomName: 房间名称
+    * userId: 请求加入房间的用户ID
+    * Version:连麦的版本号
+  */
+public function kickingPlayer($roomName,$UserId,$Version="v2"){
+    $url = sprintf("%s/%s/rooms/%s/users/%s", Config::getInstance()->RTCAPI_HOST,$Version, $roomName,$UserId);
+    try {
+        $ret = $this->_transport->send(HttpRequest::DELETE, $url);
+    } catch (\Exception $e) {
+        return $e;
+    }
+    return $ret;
+}
     /*
      * roomName: 房间名称
      * userId: 请求加入房间的用户ID
@@ -76,41 +100,10 @@ class RoomClient
         $params['user_id'] = $userId;
         $params['perm'] = $perm;
         $params['expire_at'] = $expireAt;
-
         $roomAccessString = json_encode($params);
-
         $encodedRoomAccess = Utils::base64UrlEncode($roomAccessString);
         $sign = hash_hmac('sha1', $encodedRoomAccess, $this->_mac->_secretKey, true);
         $encodedSign = Utils::base64UrlEncode($sign);
         return $this->_mac->_accessKey . ":" . $encodedSign . ":" . $encodedRoomAccess;
     }
-       /*
-     * 获取房间的人数
-      * roomName: 房间名称
-    */
-    public function getRoomUserNum($roomName){
-        $url = sprintf("%s/v1/rooms/%s/users", Config::getInstance()->RTCAPI_HOST, $roomName);
-        try {
-            $ret = $this->_transport->send(HttpRequest::GET, $url);
-        } catch (\Exception $e) {
-            return $e;
-        }
-        return $ret;
-
-    }
-    /*
-     * 踢出玩家
-    * roomName: 房间名称
-    * userId: 请求加入房间的用户ID
-  */
-public function kickingPlayer($roomName,$UserId){
-    $url = sprintf("%s/v1/rooms/%s/users/%s", Config::getInstance()->RTCAPI_HOST, $roomName,$UserId);
-    try {
-        $ret = $this->_transport->send(HttpRequest::DELETE, $url);
-    } catch (\Exception $e) {
-        return $e;
-    }
-    return $ret;
-
-}
 }
